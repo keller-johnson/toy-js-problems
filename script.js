9592fe5fc9678c10b60c117b148cf2347fb8b184
keller-johnson <keller.s.johnson@gmail.com>
2022-10-25 16:45:08 -0700

    solved find sum in array problem

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const array = [1, 2, 3, 4, 5];
 
-const findTotal = (arr, target) => {
+const findTotal = (arr, total) => {
+  let totalSum = total;
   for (let i = 0; i <= arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
-      console.log(i, "+", j, "=", i + j);
+      //console.log(i, "+", j, "=", i + j);
+      if (i + j === totalSum) {
+        console.log(i, j, totalSum);
+      }
     }
   }
 };
 
-findTotal(array);
+findTotal(array, 3);
