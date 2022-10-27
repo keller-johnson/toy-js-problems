7cc13b90c828e06d56f437d9ea3b262a7d8255ca
keller-johnson <keller.s.johnson@gmail.com>
2022-10-26 17:30:22 -0700

    solved rba hex problem for the most part

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-/* PROBLEM
-Write a javascript function that takes an array of numbers and a target number.
- The function should find two different numbers in the array that, when added together, 
- give the target number. For example: answer([1,2,3], 4)should return [1,3]
-*/
+/*Write a function that converts HEX to RGB. Then Make that function
+auto-dect the formats so that if you enter HEX color format it returns RGB
+and if you enter RGB color format it returns HEX. */
 
-const array = [1, 2, 3, 4, 5];
+let rgbToHex = (r, g, b) => {
+  // found this conversion on stackoverflow
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
 
-const findTotal = (arr, total) => {
-  let totalSum = total;
-  for (let i = 0; i <= arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      //console.log(i, "+", j, "=", i + j);
-      if (i + j === totalSum) {
-        console.log(i, j, totalSum);
+let hexToRgb = (hex) => {
+  //found this conversion on stack overflow
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
       }
-    }
+    : null;
+};
+
+let autoConvert = (x, y, z) => {
+  let ogData = x;
+  if ((x === typeof String && y, z === undefined)) {
+    console.log(hexToRgb(x));
+    console.log(`"${ogData}"`);
+  } else x, y, z === typeof Number;
+  let r = x;
+  let g = y;
+  let b = z;
+  {
+    console.log(rgbToHex(x, y, z));
+    console.log(`(${r}, ${g}, ${b})`);
   }
 };
 
-findTotal(array, 3);
+console.log(autoConvert("#ff2281"));
